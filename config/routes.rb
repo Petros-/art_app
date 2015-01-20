74570f4d92dc1cc60ddc67f6b6310bdde7cb2460
Peter Bentley <bentleyfineart@gmail.com>
2015-01-20 06:47:24 -0800

    shortened urls to peter-bentley.com/artwork-name, and created dymanic page titles for each page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  resources :artworks
+  resources :artworks, path: ''
 
   get "about" => "pages#about"
   get "artworks" => "artworks#index"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
