711c6f8f41918118ab91feb637d86d2c2f66ab8f
Peter Bentley <bentleyfineart@gmail.com>
2015-01-20 21:32:09 -0800

    tweaked styling of navbar, button padding, colors,  etc.

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,4 +1,5 @@
 class Artwork < ActiveRecord::Base
+  
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
@@ -20,4 +21,5 @@ class Artwork < ActiveRecord::Base
   def next
     Artwork.where(["id > ?", id]).first
   end
+
 end
