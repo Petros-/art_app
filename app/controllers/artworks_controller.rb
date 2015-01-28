ff0ee51be33e846672da5c35aec33b1d945d1990
Peter Bentley <bentleyfineart@gmail.com>
2015-01-28 00:18:08 -0800

    got artworks ordered by rating on the homepage

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -73,6 +73,6 @@ class ArtworksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def artwork_params
-      params.require(:artwork).permit(:title, :category, :image, :genre, :medium, :slug, :availability, :date, :height, :width)
+      params.require(:artwork).permit(:title, :category, :image, :genre, :medium, :slug, :availability, :date, :height, :width, :rating)
     end
 end
