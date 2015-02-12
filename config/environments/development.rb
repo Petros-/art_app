08bfbb155262683bb8c03f8a400b49e3605b4bd4
Peter Bentley <bentleyfineart@gmail.com>
2015-02-11 22:09:54 -0800

    trying to fix contact form.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,17 +36,15 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
+  address: "smtp.gmail.com",
   port: 587,
-  domain: ENV['GMAIL_DOMAIN'],
-  authentication: 'plain',
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV['GMAIL_USERNAME'],
-  password: ENV['GMAIL_PASSWORD']
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
   }
   
 end
